699fc59db7b77413d7316da0f0dfccd72833c945
Boatswain <son4ck.2005@list.ru>
2025-05-30 05:53:30 +0300

    Creating design and added the SplashScreen with a 3-second delay

--- app/src/main/java/com/example/onlinestore/SplashActivity.java
+++ app/src/main/java/com/example/onlinestore/SplashActivity.java
@@ -1,24 +1,29 @@
 package com.example.onlinestore;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Задержка на 3 секунды перед переходом к основному Activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Переход к основному Activity
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                // Закрытие SplashActivity
+                finish();
+            }
+        }, 3000); // 3000 миллисекунд = 3 секунды
     }
 }

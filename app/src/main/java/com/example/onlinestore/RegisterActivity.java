7af221fce43e7b93e739be6ef420c1224f9a304a
Boatswain <son4ck.2005@list.ru>
2025-06-06 19:30:50 +0300

    Implemented registration logic (RegisterActivity.java)

--- app/src/main/java/com/example/onlinestore/RegisterActivity.java
+++ app/src/main/java/com/example/onlinestore/RegisterActivity.java
@@ -2,33 +2,74 @@ package com.example.onlinestore;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.AppCompatButton;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private GoogleSignInClient googleSignInClient;
+
+    private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                    try {
+                        GoogleSignInAccount account = accountTask.getResult(ApiException.class);
+                        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                        auth.signInWithCredential(credential).addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                                finish(); // Закрываем RegisterActivity
+                            } else {
+                                Toast.makeText(this, "Ошибка входа: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    } catch (ApiException e) {
+                        Toast.makeText(this, "Ошибка авторизации: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Авторизация отменена", Toast.LENGTH_SHORT).show();
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
 
-        TextView fromRegisterToLogin = findViewById(R.id.from_register_to_login);
+        auth = FirebaseAuth.getInstance();
 
-        fromRegisterToLogin.setOnClickListener(new View.OnClickListener(){
-            public void onClick (View v) {
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+            return;
+        }
 
-                startActivity(intent);
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, options);
 
-                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
-            }
-        });
+        AppCompatButton googleRegister = findViewById(R.id.google_register);
+        googleRegister.setOnClickListener(v -> activityResultLauncher.launch(googleSignInClient.getSignInIntent()));
     }
 }

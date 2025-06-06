e366e6496b8d8b9a20065921e78ff9eea58da081
Boatswain <son4ck.2005@list.ru>
2025-06-07 00:18:57 +0300

    Update CategoryModel with new fields and accessors(getters and setters)

--- app/src/main/java/com/example/onlinestore/Domain/CategoryModel.java
+++ app/src/main/java/com/example/onlinestore/Domain/CategoryModel.java
@@ -5,4 +5,30 @@ public class CategoryModel {
     private int id;
     private String picUrl;
 
+    public CategoryModel() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
 }

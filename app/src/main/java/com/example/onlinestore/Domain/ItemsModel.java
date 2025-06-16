032ad36faaa4c61dcb91348f52dd569d35d06206
Boatswain <son4ck.2005@list.ru>
2025-06-16 13:13:02 +0300

    Added Getter and Setter of id

--- app/src/main/java/com/example/onlinestore/Domain/ItemsModel.java
+++ app/src/main/java/com/example/onlinestore/Domain/ItemsModel.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class ItemsModel implements Serializable {
+    private String id;
     private String title;
     private String description;
     private String offPercent;
@@ -17,6 +18,13 @@ public class ItemsModel implements Serializable {
     private int NumberingCart;
 
     public ItemsModel() {
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTitle() {
